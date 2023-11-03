854f514e4ec2568f71d7a3c3bff9192bd4d01111
tonynagyy <nagytony2@gmail.com>
2023-11-03 19:15:21 +0200

    added tony.cpp and mve function

---
+++ tony.cpp
@@ -0,0 +1,151 @@
+'/tmp/Rar$DRa1112.44359/Phase1 - Code F22/TestCode.cpp'
+#include "GUI\Input.h"
+#include "GUI\Output.h"
+
+//This is a test code to test the Input and Output classes
+
+int main()
+{
+	int x,y;
+
+	//Create Input and Output objects to test
+	Output *pOut = new Output();
+	Input *pIn = pOut->CreateInput();
+
+	//Starting the test
+	pOut->PrintMessage("This demo is to test input and output classes, Click anywhere to start the test");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+
+	///////////////////////////////////////////////////////////////////////////////////
+	// TEST 1:	
+	//			Create The FULL Tool bar, the drawing area and the status bar	
+	//			This has already been done through the constrcutor of class Output
+	///////////////////////////////////////////////////////////////////////////////////
+
+	pOut->PrintMessage("TEST1: Drawing Tool bar and Status bar, Click anywhere to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+	
+	///////////////////////////////////////////////////////////////////////////////////
+	// TEST 2:	
+	//			Drawing all the Figures with all possible states: 
+	//			Non-filled, Filled, and highlighted in both cases			
+	///////////////////////////////////////////////////////////////////////////////////
+	
+	pOut->PrintMessage("TEST2: Now we will show that Output class can draw any figure in any state, Click anywhere to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+	GfxInfo gfxInfo;//to be used with draw function of the class Ouput
+	Point P1, P2;
+
+	/// 2.1- Rectangle Test ///
+	/// =================== 
+	pOut->PrintMessage("Drawing a Rectangle, filled/non-filled and Highlighted filled/non-filled,  Click to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+	
+	// 2.1.1 - Drawing non-filled rectangle
+	pOut->PrintMessage("Drawing a Rectangle ==> non-filled,  Click two points");
+	pIn->GetPointClicked(P1.x, P1.y);
+	pIn->GetPointClicked(P2.x, P2.y);
+
+	gfxInfo.BorderWdth = 5;
+	gfxInfo.DrawClr = BLACK;	//any color for border
+	gfxInfo.isFilled = false;	//Figure is NOT filled
+	pOut->DrawRect(P1, P2, gfxInfo, false);
+
+	// 2.1.2 - Drawing highlighted non-filled rectangle
+	pOut->PrintMessage("Drawing a Rectangle ==> Highlighted non-filled, Click to Highlight");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+	pOut->DrawRect(P1, P2, gfxInfo, true);
+
+
+	// 2.1.3 - Drawing a filled rectangle
+	pOut->PrintMessage("Drawing a Rectangle ==> filled,  Click two points");
+	pIn->GetPointClicked(P1.x, P1.y);
+	pIn->GetPointClicked(P2.x, P2.y);
+
+	gfxInfo.BorderWdth = 6;
+	gfxInfo.DrawClr = BLUE;	//any color for border
+	gfxInfo.FillClr = GREEN;//any color for filling
+	gfxInfo.isFilled = true;//Figure is filled
+	pOut->DrawRect(P1, P2, gfxInfo, false);
+
+
+	// 2.1.4 - Drawing a highlighted filled rectangle
+	pOut->PrintMessage("Drawing a Rectangle ==> Highlighted filled, Click to Highlight");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+	pOut->DrawRect(P1, P2, gfxInfo, true);
+
+
+
+	pOut->PrintMessage("Drawing a Rectangle Test ==> OK,  Click anywhere to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+	pOut->ClearDrawArea();
+
+	/// 2.2- Square Test ///
+	/// ============== 
+	pOut->PrintMessage("Drawing a Square, normal and Highlighted, Click to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+	///TODO: Add code to draw Square, Normal and Highlighted
+
+	pOut->PrintMessage("Drawing a Square Test ==> OK,  Click anywhere to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+	pOut->ClearDrawArea();
+
+	/// 2.3- Triangle Test ///
+	/// =================== 
+	pOut->PrintMessage("Drawing a Triangle, filled/non-filled and Highlighted filled/non-filled,  Click to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+#include "GUI\Input.h"
+#include "GUI\Output.h"
+
+//This is a test code to test the Input and Output classes
+
+int main()
+{
+	int x,y;
+
+	//Create Input and Output objects to test
+	Output *pOut = new Output();
+	Input *pIn = pOut->CreateInput();
+
+	//Starting the test
+	pOut->PrintMessage("This demo is to test input and output classes, Click anywhere to start the test");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+
+	///////////////////////////////////////////////////////////////////////////////////
+	// TEST 1:	
+	//			Create The FULL Tool bar, the drawing area and the status bar	
+	//			This has already been done through the constrcutor of class Output
+	///////////////////////////////////////////////////////////////////////////////////
+
+	pOut->PrintMessage("TEST1: Drawing Tool bar and Status bar, Click anywhere to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+	
+	///////////////////////////////////////////////////////////////////////////////////
+	// TEST 2:	
+	//			Drawing all the Figures with all possible states: 
+	//			Non-filled, Filled, and highlighted in both cases			
+	///////////////////////////////////////////////////////////////////////////////////
+	
+	pOut->PrintMessage("TEST2: Now we will show that Output class can draw any figure in any state, Click anywhere to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+
+	GfxInfo gfxInfo;//to be used with draw function of the class Ouput
+	Point P1, P2;
+
+	/// 2.1- Rectangle Test ///
+	/// =================== 
+	pOut->PrintMessage("Drawing a Rectangle, filled/non-filled and Highlighted filled/non-filled,  Click to continue");
+	pIn->GetPointClicked(x,y);	//Wait for any click
+	
+	// 2.1.1 - Drawing non-filled rectangle
+	pOut->PrintMessage("Drawing a Rectangle ==> non-filled,  Click two points");
+	pIn->GetPointClicked(P1.x, P1.y);
+	pIn->GetPointClicked(P2.x, P2.y);
+
